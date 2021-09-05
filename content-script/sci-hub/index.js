0efd8ca10e30f81bdcbabd96c907085f9372a3b9
JMx <jiaminxin@outlook.com>
2021-09-05 10:19:12 +0800

    update

--- content-script/sci-hub/index.js
+++ content-script/sci-hub/index.js
@@ -1,5 +1,5 @@
 (function() {
     if( window.location.href.indexOf('sci-hub.') < 0 ) return
-    document.getElementById('article').style += "position: absolute; left: 20%; width: 60%;";
+    document.getElementById('article').style = "margin-left: 0 !important; position: absolute !important; left: 20%; width: 60%;";
     document.getElementById('menu').style.display = "none";
 })();
--- content-script/weibo-player/index.js
+++ content-script/weibo-player/index.js
@@ -4,6 +4,16 @@
     	setTimeout( setPlay, 500 );
     	return
     }
-   	let display = document.getElementsByClassName('video-player')[0].style.display;
-   	document.getElementsByClassName('video-player')[0].style = `display: ${display}; position: fixed !important;    top: 30% !important;    left: 10px !important;    width: 28% !important;    height: 40% !important`;	
+    let player = document.getElementsByClassName('video-player')[0];
+   	let display = player.style.display;
+    let style = document.createElement('style');
+    style.innerHTML = `
+      .video-player {
+        position: fixed !important;
+        top: 30% !important;
+        left: 10px !important;
+        width: 28% !important;   
+        height: 40% !important
+      }`;
+    document.body.appendChild(style)
 })()
