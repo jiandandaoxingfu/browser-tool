7e85d285fbf3a9750ba060baf14429436f3793ab
JMx <jiaminxin@outlook.com>
2022-05-23 09:10:24 +0800

    Update rename.js

--- rename.js
+++ rename.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const path = "I:/歌曲/新建文件夹 (2)/";
+const path = "C:\\Users\\Administrator\\iCloudDrive\\学习资料";
 
 class Rename {
     constructor() {
@@ -19,4 +19,21 @@ class Rename {
 }
 
 let rename = new Rename();
-rename.rename('_ev', '')
+// console.log(rename.files)
+// rename.rename('_ev', '')
+
+function fn(path){
+    fs.readdir(path,(err,files)=>{
+        files.forEach(function(item){
+            fs.stat(path+'/'+item,(err,data)=>{
+                if(data.isFile()){
+                    console.log('文件名',item);
+                }else{
+                    console.log('文件夹名',item);
+                    fn(path+'/'+item)
+                }
+            })
+        })
+    })
+}
+fn(path)
